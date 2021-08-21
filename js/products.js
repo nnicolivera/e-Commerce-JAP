8400246eb3e33dbf9233914d46f99c68d0712ad5
nnicolivera <nnicolivera@hotmail.com>
2021-08-21 02:47:38 -0300

    entrega 1
    MER, login con redirección y muestro json de productos

--- js/products.js
+++ js/products.js
@@ -3,4 +3,34 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    getJSONData(PRODUCTS_URL).then(function (resultado) {
+        if (resultado.status === "ok") {
+            autosLista = resultado.data;
+        
+            showAutos(autosLista);
+        }
+    });
+    
 });
+
+
+let autosLista = [];
+
+function showAutos(array) {
+
+    let listado = "";
+    for (let i = 0; i < array.length; i++) {
+        let auto = array[i];
+
+        listado += `<img src= "` +  auto.imgSrc + `" width="25%" class="img-thumbnail thumbnail">` + "<br>";
+        listado += `
+                <div class="texto">
+                ` + "<strong>" + auto.name + "</strong>" + ` <br>
+                ` + auto.description + ` <br>
+                ` + auto.currency + ` ` + auto.cost + "." + ` <br><hr>
+                </div>
+                ` 
+
+    }
+    document.getElementById("listado").innerHTML = listado;
+}
