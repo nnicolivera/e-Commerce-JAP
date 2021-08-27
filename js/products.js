78980c73d54ece3d7c6b1338e5f69ef4484ddf4d
Nicolás <nnicolivera@hotmail.com>
2021-08-27 01:42:07 -0300

    correcciones generales

--- js/products.js
+++ js/products.js
@@ -3,4 +3,34 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    getJSONData(PRODUCTS_URL).then(function (resultado) {
+        if (resultado.status === "ok") {
+            autosLista = resultado.data;
+        
+            showAutos(autosLista);
+        }
+    });
+    
 });
+
+
+let autosLista = [];
+
+function showAutos(array) {
+
+    let datos = "";
+    for (let i = 0; i < array.length; i++) {
+        let auto = array[i];
+
+        datos += `<img src= "` +  auto.imgSrc + `" width="22%" class="img-thumbnail thumbnail">`;
+        datos += `
+                <div class="texto">
+                ` + "<strong>" + auto.name + "</strong>" + ` <br>
+                ` + auto.description + ` <br>
+                ` + auto.currency + ` ` + auto.cost + "." + ` <br><hr>
+                </div>
+                ` 
+
+    }
+    document.getElementById("listado").innerHTML = datos;
+}
