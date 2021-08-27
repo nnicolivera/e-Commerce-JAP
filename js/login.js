fff126759225361d01bdfc3f87a4d9b235d0c38a
Nicolás <nnicolivera@hotmail.com>
2021-08-27 01:24:36 -0300

    entrega 1 completa

--- js/login.js
+++ js/login.js
@@ -2,5 +2,26 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+        
+    document.getElementById("inputButton").addEventListener("click", function(e){
+        
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPass");
+        let camposCompletos = true;
 
+        if(inputEmail.value === ""){
+            camposCompletos = false;
+            alert("Ingresar correo electrónico");
+        }
+            
+        if(inputPassword.value === ""){
+            camposCompletos = false;
+            alert("Ingresar contraseña");
+        }
+        
+        if(camposCompletos){
+            window.location = "inicio.html";
+        }
+        
+    })
 });
