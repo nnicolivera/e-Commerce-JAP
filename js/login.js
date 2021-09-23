2c3a3369a9c13876357c5085642138bf79ccb002
Nicolás <nnicolivera@hotmail.com>
2021-09-22 23:56:10 -0300

    update termsfeed links

--- js/login.js
+++ js/login.js
@@ -1,28 +1,45 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-        
-    document.getElementById("entrar").addEventListener("click", function(e){
-        
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    document.getElementById("entrar").addEventListener("click", function (e) {
+
         let inputEmail = document.getElementById("inputEmail");
         let inputPassword = document.getElementById("inputPass");
         let camposCompletos = true;
 
-        if(inputEmail.value === ""){
+        if (inputEmail.value === "") {
             camposCompletos = false;
             alert("Ingresar correo electrónico");
         }
-            
-        if(inputPassword.value === ""){
+
+        if (inputPassword.value === "") {
             camposCompletos = false;
             alert("Ingresar contraseña");
         }
-        
-        if(camposCompletos){
+
+        if (camposCompletos) {
             window.location = "inicio.html";
-            localStorage.setItem('User-Logged', JSON.stringify({email: inputEmail.value}));
+            localStorage.setItem('User-Logged', JSON.stringify({ email: inputEmail.value }));
         }
-        
+
     })
 });
+
+//INICIAR SESIÓN CON GOOGLE (EN PROGRESO)
+function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+}
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "801182614848-fclhb3ime35fmiceppdeaa94c9ue5kqa.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+}
+//INICIAR SESIÓN CON GOOGLE (EN PROGRESO)
