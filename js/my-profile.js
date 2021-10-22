809903404b2a924257b449627ddf3815c2352989
Nicolás <nnicolivera@hotmail.com>
2021-10-22 02:22:24 -0300

    entrega 6 FULL
    ahora el usuario puede guardar su imagen de perfil también, igual debo ajustar su tamaño

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,5 +1,4 @@
-var test = document.getElementById("test");
-
+var imgAsDataURL = "";
 var userName = document.getElementById("userName");
 var userSurname = document.getElementById("userSurname");
 var userAge = document.getElementById("userAge");
@@ -18,7 +17,7 @@ userEmail.value = userL.value;
 (function() {
   'use strict';
   window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Se hace un fetch a todos los formularios que queremos aplicarles las clases de validación con estilos de Bootstrap
     var forms = document.getElementsByClassName('needs-validation');
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
@@ -28,6 +27,7 @@ userEmail.value = userL.value;
           event.stopPropagation();
         }
         form.classList.add('was-validated');
+        localStorage.setItem("User-Image", imgAsDataURL);
         localStorage.setItem('User-Name', JSON.stringify({ value: userName.value }));
         localStorage.setItem('User-Surname', JSON.stringify({ value: userSurname.value }));
         localStorage.setItem('User-Birth', JSON.stringify({ value: userBirth.value }));
@@ -36,6 +36,7 @@ userEmail.value = userL.value;
         localStorage.setItem('User-Phone', JSON.stringify({ value: userPhone.value }));
       }, false);
 
+        let userI = localStorage.getItem('User-Image');
         let userN = localStorage.getItem('User-Name');
         let userS = localStorage.getItem('User-Surname');
         let userB = localStorage.getItem('User-Birth');
@@ -50,7 +51,8 @@ userEmail.value = userL.value;
         userL = JSON.parse(userL);
         userP = JSON.parse(userP);
 
-        if (userN && userS && userB && userA && userL && userP) {
+        if (userI && userN && userS && userB && userA && userL && userP) {
+          userImage.src = userI;
           userName.value = userN.value;
           userSurname.value = userS.value;
           userBirth.value = userB.value;
@@ -60,13 +62,36 @@ userEmail.value = userL.value;
         }
 
       document.getElementById('remove').addEventListener('click', function() {
+        localStorage.removeItem('User-Image')
         localStorage.removeItem('User-Name');
         localStorage.removeItem('User-Surname');
         localStorage.removeItem('User-Birth');
         localStorage.removeItem('User-Age');
-        localStorage.removeItem('User-Email');
         localStorage.removeItem('User-Phone');
       });
     });
   }, false);
 })();
+
+
+var loadPhoto = function(event) {
+	var userImage = document.getElementById('userImage');
+	userImage.src = URL.createObjectURL(event.target.files[0]);
+};
+
+// Take action when the image has loaded
+userImage.addEventListener("load", function () {
+    var imgCanvas = document.createElement("canvas"),
+        imgContext = imgCanvas.getContext("2d");
+
+    // Make sure canvas is as big as the picture
+    imgCanvas.width = userImage.width;
+    imgCanvas.height = userImage.height;
+
+    // Draw image into canvas element
+    imgContext.drawImage(userImage, 0, 0, imgCanvas.width, imgCanvas.height);
+
+    // Get canvas contents as a data URL
+    imgAsDataURL = imgCanvas.toDataURL("image/png");
+
+}, false); 
